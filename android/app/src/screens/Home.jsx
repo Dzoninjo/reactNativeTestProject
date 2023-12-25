d0239c57659a38e2ce34e12d79fc95eaf8190731
Dzoninjo <nikolajohnie116@gmail.com>
2023-12-25 15:49:55 +0100

    napravljena sqlite baza

--- android/app/src/screens/Home.jsx
+++ android/app/src/screens/Home.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Alert } from 'react-native';
 import GlobalStyle from '../utils/GlobalStyle';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 import { TextInput } from 'react-native-gesture-handler';
 import CustomButton from '../utils/CustomButton';
+import SQLite from 'react-native-sqlite-storage'
+
+const db = SQLite.openDatabase({
+  name:'MainDB',
+  location:'default',
+},
+() =>{},
+error => {console.log(error)}
+)
 
 export default function Home({navigation, route}) {
 
@@ -11,62 +20,91 @@ export default function Home({navigation, route}) {
   const [age,setAge] = useState("");
 
   useEffect(()=>{
+    createTable();
     getData();
   },[]);
 
+  const createTable = () =>{
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS "
+        +"Users "
+        +"(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
+      )
+    })
+  }
+
   const getData = () =>{
     try{
-      AsyncStorage.getItem('UserData')
-      .then(value=>{
-        if(value !=null){
-          let user = JSON.parse(value);
-          setName(user.Name);
-          setAge(user.Age);
-        }
-      }
+      // AsyncStorage.getItem('UserData')
+      // .then(value=>{
+      //   if(value !=null){
+      //     let user = JSON.parse(value);
+      //     setName(user.Name);
+      //     setAge(user.Age);
+      //   }
+      // }
 
-      )
+      // )
+      db.transaction((tx)=>{
+        tx.executeSql(
+          "SELECT Name, Age FROM Users",
+          [],
+          (tx,results) =>{
+            var len = results.rows.length;
+            if (len > 0){
+              var userName = results.rows.item(0).Name;
+              var userAge = results.rows.item(0).Age;
+              setName(userName);
+              setAge(userAge);
+            }
+          }
+        )
+      })
     }
     catch(error){
       console.log(error);
     }
   }
 
-  const updateData = async () => {
-    if(name.length == 0){
-        Alert.alert("Upozorenje", "Molimo Vas da unesete vase podatke")
-    }
-    else{
-        try{
-          var user ={
-            Name:name,
-          }
-            await AsyncStorage.mergeItem("UserData", JSON.stringify(user));
-            Alert.alert("Cestitamo", "Uspesno ste azurirali podatke");
-
-        }
-        catch(error){
-         console.log(error);
-        }
-    }
-}
-
-const removeData = async () => {
-      try{
-          await AsyncStorage.removeItem("UserData");
-          navigation.navigate("Login");
-
+  const updateData = () => {
+    if (name.length === 0) {
+      Alert.alert("Upozorenje", "Molimo Vas da unesete vase podatke")
+    } else {
+      try {
+         db.transaction( (tx) => {
+          tx.executeSql(
+            "UPDATE Users SET Name=?",
+            [name],
+            () => {Alert.alert("Cestitamo", "Uspesno ste azurirali podatke")},
+            error => {console.log(error)}
+          );
+        });
+      } catch (error) {
+        console.log(error);
       }
-      catch(error){
-       console.log(error);
-      }
-}
+    }
+  }
+  
+  const removeData =  () => {
+    try {
+       db.transaction((tx) => {
+        tx.executeSql("DELETE FROM Users",
+        [],
+        () => {navigation.navigate("Login")},
+        error => {console.log(error)}
+        );
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
     return (
       <View style={styles.body}>
         <Text style = {[
           GlobalStyle.CustomFont,
-          styles.text]}>Welcome {name}</Text>
+          styles.text]}>Dobrodosli {name}!</Text>
         <Text style = {[
           GlobalStyle.CustomFont,
           styles.text]}>Vase godine: {age}</Text>
--- android/app/src/screens/Login.jsx
+++ android/app/src/screens/Login.jsx
@@ -6,7 +6,17 @@ import { StyleSheet,
     Alert
 } from "react-native";
 import CustomButton from "../utils/CustomButton";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+import SQLite from 'react-native-sqlite-storage';
+
+const db = SQLite.openDatabase(
+    {
+        name : 'MainDB',
+        location : 'default',
+    },
+    ()=>{},
+    error => {console.log(error)}
+);
 
 export default function Login({navigation}){
 
@@ -14,22 +24,64 @@ export default function Login({navigation}){
     const [age, setAge] = useState('');
 
     useEffect(()=>{
-        setData();
+        createTable();
+        getData();
+        // setData();
     },[])
 
+const createTable = () =>{
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS "
+        +"Users "
+        +"(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
+      )
+    })
+  }
+
+  const getData = () =>{
+    try{
+     AsyncStorage.getItem("UserData")
+     .then(value => {
+        if (value => null){
+            navigation.navigate("Home");
+        }
+     })
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+
     const setData = async () => {
-        if(name.length == 0 || age.length == 0){
+        if(name.length === 0 || age.length === 0){
             Alert.alert("Upozorenje", "Molimo Vas da unesete vase podatke")
         }
         else{
             try{
-                var user = {
-                    Name:name,
-                    Age:age
-                }
-                await AsyncStorage.setItem("UserData", JSON.stringify(user));
-                navigation.navigate('Home');
+                // var user = {
+                //     Name:name,
+                //     Age:age
+                // }
+                // await AsyncStorage.setItem("UserData", JSON.stringify(user));
+                // navigation.navigate('Home');
+                await db.transaction(async (tx)=>{
+                    await tx.executeSql(
+                        "INSERT INTO Users (Name,Age) VALUES (?,?)",
+                        [name,age]);
+                })
+                navigation.navigate("Home");
 
+                tx.executeSql(
+                    "SELECT Name, Age FROM Users",
+                    [],
+                    (tx,results) =>{
+                      var len = results.rows.length;
+                      if (len > 0){
+                       navigation.navigate("Home");
+                      }
+                    }
+                  )
             }
             catch(error){
              console.log(error);
